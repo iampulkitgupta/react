619d4c6f2e8e82e38673e66621c2e9fe914acd7b
Pulkit Gupta <pulkit.noida@gmail.com>
2025-11-02 16:36:38 +0530

    2 projects, Basic and Vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+import Chai from './Chai'
+
+function App() {  
+
+  return (
+    <h1>React Learning with vite</h1>    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,8 @@
+function Chai() {  
+
+  return (
+    <h1>React Learning with chai</h1>    
+  )
+}
+
+export default Chai

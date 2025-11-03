368ebf1b4c7e8f3579edb8d9f2d03a11fbb62c27
Pulkit Gupta <pulkit.noida@gmail.com>
2025-11-03 21:08:41 +0530

    counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(10)
+
+  // let counter = 15
+
+  const addValue = () => {
+    if (counter === 20){
+
+    }    
+    else setCounter(counter+1)     
+  }
+
+  const removeValue = () => {
+    if (counter === 0){
+
+    }
+    else setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>React Learning</h1>
+      <h1>Counter Value: {counter}</h1>
+
+      <button 
+      onClick={addValue}
+      >Add value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove Value</button>
+    </>
+  )
+}
+
+export default App
